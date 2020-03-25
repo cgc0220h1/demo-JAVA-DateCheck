934826ed3f122b80c74f23e86793be12227c303f
vanduc2514 <vanduc2514@gmail.com>
2020-03-25 20:30:51 +0700

    Thêm test case

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        //Test năm nhuận
         Date date = new Date(12,3,2016);
         System.out.println(date.toString());
         System.out.println("Is Leap year: " + date.isLeapYear());
+
+        //Test năm không nhuận
+        Date date1 = new Date(31,12,2002);
+        System.out.println(date1.toString());
+        System.out.println("Is Leap year: " + date1.isLeapYear());
     }
 }
