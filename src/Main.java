78917251703d8afdd3bbbe9f36829c83acfb8a1d
vanduc2514 <vanduc2514@gmail.com>
2020-03-25 20:28:51 +0700

    Add code

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date(12,3,2016);
+        System.out.println(date.toString());
+        System.out.println("Is Leap year: " + date.isLeapYear());
+    }
+}
